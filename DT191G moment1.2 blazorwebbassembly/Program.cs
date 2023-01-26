958fc26008d8e193caafc418ace200515554ef95
Natalie Frick <54511498+nataliefrick@users.noreply.github.com>
2023-01-26 10:39:24 +0100

    Add project files.

---
+++ DT191G moment1.2 blazorwebbassembly/Program.cs
@@ -0,0 +1,11 @@
+using DT191G_moment1._2_blazorwebbassembly;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
